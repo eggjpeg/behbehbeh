035ae010087245695eff3844a1c2989c38d8538a
eggjpeg <kolya46592@gmail.com>
2023-08-16 15:11:46 -0400

    added a visitor class

---
+++ behbehbeh/CQLVisitor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace behbehbeh
+{
+    using Antlr4.Runtime;
+
+        public class CqlVisitor : CqlGrammarBaseVisitor<object>
+        {
+            public override object VisitSelectStatement(CqlGrammarParser.SelectStatementContext context)
+            {
+                // Logic for visiting and processing SELECT statements
+                return base.VisitSelectStatement(context);
+            }
+
+            public override object VisitWhereClause(CqlGrammarParser.WhereClauseContext context)
+            {
+                // Logic for visiting and processing WHERE clauses
+                return base.VisitWhereClause(context);
+            }
+
+            // Add more overridden methods for other grammar rules as needed
+
+            // Example entry point to start visiting the parse tree
+            public void VisitParseTree(CqlGrammarParser.ParseContext context)
+            {
+                Visit(context);
+            }
+        }
+
+    }
+
